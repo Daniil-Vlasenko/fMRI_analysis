2254ed1a768b9e2a716e167c04f31afde4a0c8f8
Daniil Vlasenko <daniil_vlasenko01@mail.ru>
2023-01-23 16:19:23 +0300

    .

--- code/main.py
+++ code/main.py
@@ -7,32 +7,22 @@ import scipy.stats as stat
 import pearson_spearman as cor
 import igraph as ig
 import synolitic
-import preprocessed_data
+import processed_data
 from pathlib import Path
 import nilearn.image as image
 
 
-print(len(data.imagery))
-print(len(data.perception))
-print(len(data.imagery_test))
-print(len(data.perception_test))
-print(len(data.imagery_training))
-print(len(data.perception_training))
+print(len(processed_data.imagery))
+print(len(processed_data.perception))
+print(len(processed_data.imagery_test))
+print(len(processed_data.perception_test))
+print(len(processed_data.imagery_training))
+print(len(processed_data.perception_training))
 
-iput_files = data.all_files
-n = 10
-for i in iput_files:
-    img = image.load_img(i)
-    affine = img.affine.copy()
-    print(i)
-    print(affine)
-    for j in range(3):
-        affine[j][j] = n * np.sign(affine[j][j])
-    new_img = image.resample_img(img, affine)
-    path_array = i.split("/")
-    new_path = "../processed_data/dimensionality_reduction_1/" + path_array[5] + "/" + path_array[6] + "/" + path_array[
-        8]
-    nib.save(new_img, new_path)
+for i in processed_data.all_files:
+    path = Path(i)
+    if not path.is_file():
+        print(i)
 
 
 
