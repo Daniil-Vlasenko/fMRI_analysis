9d7ae11be9ed0fb6d095715c37627a1acc096008
Daniil Vlasenko <daniil_vlasenko01@mail.ru>
2023-02-23 20:53:38 +0300

    .

--- code/main.py
+++ code/main.py
@@ -25,19 +25,27 @@ shape = (20, 20, 16, 201)
 # import scalarization
 #
 # for i in range(8):
-#     cl.classifier_learning_1_SVC(perception_file=scalars.scalars_test_10_10_10[i][0], imagery_file=scalars.scalars_test_10_10_10[i][1],
+#     cl.classifier_learning_1_SVC(perception_file=scalars.scalars_tr_10_10_10[i][0], imagery_file=scalars.scalars_tr_10_10_10[i][1],
 #                                  shape=shape, classifiers_folder=classifiers.classifiers[i])
 # print("classifier_learning_1_SVC end")
 # for i in range(8):
 #     graph_generation_lib.edges_calculation_1(classifiers_folder=classifiers.classifiers[i],
+#                                              perception_file=scalars.scalars_tr_10_10_10[i][0], imagery_file=scalars.scalars_tr_10_10_10[i][1],
+#                                              shape=shape, edges_per_file=edges.edges_tr_10_10_10[i][0], edges_ig_file=edges.edges_tr_10_10_10[i][1])
+# for i in range(8):
+#     graph_generation_lib.edges_calculation_1(classifiers_folder=classifiers.classifiers[i],
 #                                              perception_file=scalars.scalars_test_10_10_10[i][0], imagery_file=scalars.scalars_test_10_10_10[i][1],
 #                                              shape=shape, edges_per_file=edges.edges_test_10_10_10[i][0], edges_ig_file=edges.edges_test_10_10_10[i][1])
-# print("edges_calculation_1 end")
-# for i in range(8):
-#     graph_generation_lib.graphs_generation(perception_file=scalars.scalars_test_10_10_10[i][0], imagery_file=scalars.scalars_test_10_10_10[i][1],
-#                                            shape=shape, edges_per_file=edges.edges_test_10_10_10[i][0], edges_ig_file=edges.edges_test_10_10_10[i][1],
-#                                            graph_per_folder=graphs.graphs_test_10_10_10[i][0], graph_im_folder=graphs.graphs_test_10_10_10[i][1])
-# print("graphs_generation end")
+print("edges_calculation_1 end")
+for i in range(8):
+    graph_generation_lib.graphs_generation(perception_file=scalars.scalars_tr_10_10_10[i][0], imagery_file=scalars.scalars_tr_10_10_10[i][1],
+                                           shape=shape, edges_per_file=edges.edges_tr_10_10_10[i][0], edges_ig_file=edges.edges_tr_10_10_10[i][1],
+                                           graph_per_folder=graphs.graphs_tr_10_10_10[i][0], graph_im_folder=graphs.graphs_tr_10_10_10[i][1])
+for i in range(8):
+    graph_generation_lib.graphs_generation(perception_file=scalars.scalars_test_10_10_10[i][0], imagery_file=scalars.scalars_test_10_10_10[i][1],
+                                           shape=shape, edges_per_file=edges.edges_test_10_10_10[i][0], edges_ig_file=edges.edges_test_10_10_10[i][1],
+                                           graph_per_folder=graphs.graphs_test_10_10_10[i][0], graph_im_folder=graphs.graphs_test_10_10_10[i][1])
+print("graphs_generation end")
 # Удали графы из папки, если размер выборки изменился.
 for i in range(8):
     ga.graphs_weight_features(graph_per_folder=graphs.graphs_tr_10_10_10[i][0], graph_im_folder=graphs.graphs_tr_10_10_10[i][1],
@@ -46,10 +54,10 @@ for i in range(8):
 for i in range(8):
     ga.graphs_weight_features(graph_per_folder=graphs.graphs_test_10_10_10[i][0], graph_im_folder=graphs.graphs_test_10_10_10[i][1],
                               features_per_file=features.features_test_10_10_10[i][0], features_im_file=features.features_test_10_10_10[i][1])
-
-# for i in range(8):
-#     print(i, test.accuracy_weight(features.features_tr_10_10_10[i][0], features.features_tr_10_10_10[i][1],
-#                                   features.features_test_10_10_10[i][0], features.features_test_10_10_10[i][1]))
+print("graphs_weight_features end")
+for i in range(8):
+    print(i, test.accuracy_weight(features.features_tr_10_10_10[i][0], features.features_tr_10_10_10[i][1],
+                                  features.features_test_10_10_10[i][0], features.features_test_10_10_10[i][1]))
 
 
 
